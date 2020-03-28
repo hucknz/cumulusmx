4d481879c4adb8e39adca446fceb0fbc19f23aa2
OptoIsolated IO <46586989+optoisolated@users.noreply.github.com>
2020-03-29 01:08:38 +1000

    Initial Commit
    Added rebuild file which can be used in a cron job to rebuild the container (checking for any updated releases and rebuilding it with that release).

---
+++ update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /opt/MXWeather
+docker stop MXWeather && docker rm MXWeather
+docker build -t ubuntu:MXWeather .
+docker run --name=MXWeather -p 8998:8998 -p 8080:80 -v /opt/MXWeather/data:/opt/CumulusMX/data -v /opt/MXWeather/backup:/opt/CumulusMX/backup -v /opt/MXWeather/log:/var/log/nginx -v /opt/MXWeather/Cumulus.ini:/opt/CumulusMX/Cumulus.ini --privileged -d ubuntu:MXWeather
