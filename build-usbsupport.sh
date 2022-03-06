e127976261cea49480179930e5196d63280d787c
OptoIsolated IO <46586989+optoisolated@users.noreply.github.com>
2022-03-07 09:40:39 +1100

    Added MXdiags support

--- build-usbsupport.sh
+++ build-usbsupport.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 cd /opt/MXWeather
 docker stop MXWeather && docker rm MXWeather
-#docker build --no-cache -t ubuntu:MXWeather .
 docker build -t ubuntu:MXWeather .
 docker run --name=MXWeather -p 8998:8998 -p 8080:80 \
   -v /opt/MXWeather/data:/opt/CumulusMX/data \
   -v /opt/MXWeather/backup:/opt/CumulusMX/backup \
   -v /opt/MXWeather/log:/var/log/nginx \
+  -v /opt/MXWeather/MXdiags:/opt/CumulusMX/MXdiags \
   -v /opt/MXWeather/Cumulus.ini:/opt/CumulusMX/Cumulus.ini \
-  --device=/dev/hidraw0 -d ubuntu:MXWeather
-docker stop MXWeather && sleep 5 && docker start MXWeather
+  --device=/dev/hidraw0  \
+  -d ubuntu:MXWeather
+docker restart MXWeather
