6f8eee3e41b8170852e63fd7a05eb0f3b408848a
OptoIsolated IO <46586989+optoisolated@users.noreply.github.com>
2022-03-08 02:11:55 +1100

    Changed public website port to 80

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 cd /opt/MXWeather
 docker stop MXWeather && docker rm MXWeather
 docker build -t ubuntu:MXWeather .
-docker run --name=MXWeather -p 8998:8998 -p 8080:80 \
+docker run --name=MXWeather -p 8998:8998 -p 80:80 \
   -v /opt/MXWeather/data:/opt/CumulusMX/data \
   -v /opt/MXWeather/backup:/opt/CumulusMX/backup \
   -v /opt/MXWeather/log:/var/log/nginx \
