2effe22f386808b871e1dc378ddd69e599b2a567
OptoIsolated IO <46586989+optoisolated@users.noreply.github.com>
2022-03-07 22:10:59 +1100

    Improved Cumulus.ini retention handling
    Container config file is now retained outside container rebuilds without clunky direct docker file mapping.

--- MXWeather.sh
+++ MXWeather.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 service nginx start
-mono /opt/CumulusMX/CumulusMX.exe >> /var/log/nginx/CumulusMX.log
+
+set -x
+pid=0
+
+# SIGTERM-handler
+term_handler() {
+  if [ $pid -ne 0 ]; then
+    kill -SIGTERM "$pid"
+    wait "$pid"
+    sleep 2
+    cp -f /opt/CumulusMX/Cumulus.ini /opt/CumulusMX/config/
+  fi
+  exit 143; # 128 + 15 -- SIGTERM
+}
+
+# setup handlers
+trap 'kill ${!}; term_handler' SIGTERM
+
+# run application
+cp -f /opt/CumulusMX/config/Cumulus.ini /opt/CumulusMX/
+mono /opt/CumulusMX/CumulusMX.exe >> /var/log/nginx/CumulusMX.log &
+pid="$!"
+
+# wait forever
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
