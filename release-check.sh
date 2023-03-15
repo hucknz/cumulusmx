d9b5be07810c180eee3943fba6a718ef54e2c6ea
hucknz <12265772+hucknz@users.noreply.github.com>
2023-03-16 12:05:32 +1300

    Create release-check.sh

---
+++ release-check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+release=$(curl -sL 'https://api.github.com/repos/cumulusmx/CumulusMX/releases/latest' 2>/dev/null)
+
+release_version=$(echo $release | jq .tag_name -r)
+
+if [ -z "$release_version" ] || [ "$release_version" != "" ]
+  then
+    echo "$release_version" > upstream-releases/cumulusmx-latest.txt
+fi
