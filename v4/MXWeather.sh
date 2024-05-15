9802130b37df935a58aff0e500751e442e27c6e5
Hamish Riley <12265772+hucknz@users.noreply.github.com>
2024-05-15 16:00:36 +1200

    Add migration script

--- v4/MXWeather.sh
+++ v4/MXWeather.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+# Migrate v3 to v4 functionality
+
+# Checks if there is more than 1 file in the data folder (indicates new install or existing)
+if [ "$(ls -A /opt/CumulusMX/data/ | wc -l)" -gt 1 ]; then 
+# Checks for datav3 folder. If it doesn't exist then creates it and copies v3 files to it and makes a backup. 
+    if [ ! -f "/opt/CumulusMX/config/.migrated" ]; then 
+      # Backup v3 Cumulus.ini
+      cp -f /opt/CumulusMX/config/Cumulus.ini /opt/CumulusMX/config/Cumulus.ini.v3.bak
+      # Copy Cumulus.ini to root
+      cp -f /opt/CumulusMX/config/Cumulus.ini /opt/CumulusMX/
+      # Backup data files
+      mkdir -p /opt/CumulusMX/backup/datav3
+      cp -R /opt/CumulusMX/data/* /opt/CumulusMX/backup/datav3
+      # Copy data files to datav3 for migration
+      mkdir /opt/CumulusMX/datav3
+      cp -R /opt/CumulusMX/data/* /opt/CumulusMX/datav3
+      # Run migration script
+      expect <<EOF
+spawn dotnet MigrateData3to4.dll
+expect "Press a Enter to continue, or Ctrl-C to exit"
+send "\r"
+expect "Press Enter to exit"
+send "\r"
+expect eof
+EOF
+      # Leave a file to indicate the migration has been completed
+      touch /opt/CumulusMX/config/.migrated
+    else 
+      # If the .migrated file already exists it skips the migration. 
+      echo "Migration already completed."
+    fi
+else
+  # No data detected so there's nothing to migrate. 
+    echo "No data detected. Skipping migration."
+fi
+
 # Start nginx web server
 service nginx start
 
