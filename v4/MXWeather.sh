6bc47f2b5a5f791e087cd47423c8013a5cc00c37
Hamish Riley <12265772+hucknz@users.noreply.github.com>
2024-05-17 16:30:32 +1200

    Update to v4

--- v4/MXWeather.sh
+++ v4/MXWeather.sh
@@ -4,50 +4,72 @@
 
 # Enables migration if the environment variable is set
 if [ "$MIGRATE" != "false" ]; then
-echo "Migration enabled. Checking if migration has already been completed..."
+echo "Migration enabled. Begin migration checks..."
+
   # Checks if there is more than 1 file in the data folder (indicates new install or existing)
-  if [ "$(ls -A /opt/CumulusMX/data/ | wc -l)" -gt 1 ]; then 
-    # Checks to see if data has already been migrated and skips if it has. 
-    if [ ! -f "/opt/CumulusMX/config/.migrated" ]; then 
-      echo "Migration has not been completed. Starting migration..."
+  if [ "$(ls -A /opt/CumulusMX/data/ | wc -l)" -gt 1 ]|| [ "$MIGRATE" == "force" ]; then 
+  echo "Multiple files detected. Checking if migration has already been completed..."
+
+    # Checks to see if data has already been migrated and skips migration if it has. 
+    if [ ! -f "/opt/CumulusMX/config/.migrated" ] && [ ! -f "/opt/CumulusMX/config/.nodata" ] || [ "$MIGRATE" == "force" ]; then 
+      if [ "$MIGRATE" == "force" ]; then
+        echo "Migration is being forced. Backing up files..."
+      else 
+       echo "No previous migration detected. Backing up files..."
+      fi
+
       # Backup Cumulus.ini file
       cp -R /opt/CumulusMX/config/Cumulus.ini /opt/CumulusMX/config/Cumulus-v3.ini.bak
       echo "Backed up Cumulus.ini"
-      # Copy Cumulus.ini to root
-      cp -f /opt/CumulusMX/config/Cumulus.ini /opt/CumulusMX/
+
       # Backup data files
       mkdir -p /opt/CumulusMX/backup/datav3
       cp -R /opt/CumulusMX/data/* /opt/CumulusMX/backup/datav3
       echo "Backed up data folder"
+
+      # Copy Cumulus.ini to root
+      cp -f /opt/CumulusMX/config/Cumulus.ini /opt/CumulusMX/
+      echo "Copied Cumulus.ini file to root"
+
       # Copy data files to datav3 for migration
       mkdir /opt/CumulusMX/datav3
       cp -R /opt/CumulusMX/data/* /opt/CumulusMX/datav3
       echo "Copied data files to migration folder"
+
       # Run migration script
-    expect <<EOF
-spawn dotnet MigrateData3to4.dll
+      echo "Running migration task..."
+      expect <<EOF
+spawn dotnet MigrateData3to4.dll $MIGRATE_CUSTOM_LOG_FILES
 expect "Press a Enter to continue, or Ctrl-C to exit"
 send "\r"
 expect "Press Enter to exit"
 send "\r"
 expect eof
 EOF
+
       # Leave a file to indicate the migration has been completed
       touch /opt/CumulusMX/config/.migrated
-      # Copy UniqueID file to config folder
-      cp -f /opt/CumulusMX/UniqueId.txt /opt/CumulusMX/config/
+
+      # Copy UniqueID file to config folder if it exists
+
+      if [ -f "/opt/CumulusMX/UniqueId.txt" ]; then
+        cp -f /opt/CumulusMX/UniqueId.txt /opt/CumulusMX/config/
+      fi
       echo "Migration completed. Starting CumulusMX..."
+
     else 
-      # If the .migrated file already exists it skips the migration. 
-      echo "Migration already completed... Skipping migration."
+      # If the .migrated or .nodata file already exists it skips the migration. 
+      echo "Migration has already been done... Skipping migration."
     fi
+
   else
     # No data detected so there's nothing to migrate. Leave a file to indicate the migration has been completed. 
-    touch /opt/CumulusMX/config/.skipmigration
-    echo "No data detected. Skipping migration."
+    touch /opt/CumulusMX/config/.nodata
+    echo "No data detected... Skipping migration."
   fi
+  
 else
-  echo "Migration not enabled... Skipping migration."
+  echo "Migration is disabled... Skipping migration."
 fi
 
 # Start NGINX web server
